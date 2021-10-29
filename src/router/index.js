ba2cd2eaa50f11bb3ed06a9130697869d575d959
D.Boy.Blade <ykw0104@163.com>
2021-10-29 10:16:09 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    // path: "/",
+    // name: "Home",
+    // component: Home,
   },
 ];
 
